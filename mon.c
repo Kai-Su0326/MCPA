6ebafbc37ecaca5b0fb3a0e46bd2cf83655991c7
Steven Bell <botsnlinux@gmail.com>
2019-10-16 21:34:55 -0400

    Initial commit

---
+++ mon.c
@@ -0,0 +1,30 @@
+#include "mon.h"
+
+Element* create_left(Element* root, char* data)
+{
+
+  return NULL;
+}
+
+Element* create_right(Element* root, char* data)
+{
+
+  return NULL;
+}
+
+void free_tree(Element* root)
+{
+
+}
+
+int write_tree(FILE* f, Element* e)
+{
+
+  return 0;
+}
+
+Element* load_tree(FILE* f)
+{
+
+  return NULL;
+}
